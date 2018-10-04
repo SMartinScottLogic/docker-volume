d534cd82b5f3f2f274850eb564c5e6def847aa8b
Simon Martin <havvoric@gmail.com>
2018-10-04 09:34:29 +0100

    Add stubs for all volume endpoints.

--- driver/driver.js
+++ driver/driver.js
@@ -17,6 +17,38 @@ app.post('/Plugin.Activate', (req, res) => {
 	console.log(req.body)
 	res.send({Implements: ["VolumeDriver"]})
 })
+app.post('/VolumeDriver.Create', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Remove', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Mount', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Path', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Unmount', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Get', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.List', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
+app.post('/VolumeDriver.Capabilities', (req, res) => {
+	console.log(req.body)
+	res.send({})
+})
 app.post('*', (req, res) => {
   console.log(req.body)
   res.send('Gotcha: ' + req.body.message || '[missing]')
