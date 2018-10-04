3b2da00ed99b1f0d17f4eab427d802371d4662ce
Simon Martin <havvoric@gmail.com>
2018-10-04 09:21:44 +0100

    Implement Activate endpoint

--- driver/driver.js
+++ driver/driver.js
@@ -13,6 +13,10 @@ app.use( (req, res, next) => {
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.post('/Plugin.Activate', (req, res) => {
+	console.log(req.body)
+	res.send({Implements: ["VolumeDriver"]})
+})
 app.post('*', (req, res) => {
   console.log(req.body)
   res.send('Gotcha: ' + req.body.message || '[missing]')
