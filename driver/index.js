0c13228a1f5b8de1f9150600fcaaef59d49a995c
Simon Martin <havvoric@gmail.com>
2018-10-04 08:40:12 +0100

    Add stub express server for volume driver.

---
+++ driver/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set("json spaces", 2)
+app.use(bodyParser.json())
+app.use( (req, res, next) => {
+    console.info(req.method, req.originalUrl)
+    console.info("content", req.body)
+    next();
+})
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.post('*', (req, res) => {
+  console.log(req.body)
+  res.send('Gotcha: ' + req.body.message || '[missing]')
+})
+app.listen(4449)
+
